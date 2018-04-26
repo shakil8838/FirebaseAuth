4c84bb1bc03c35bcfd1b49d043d121a1ac2f7d48
Md Shakil Ahmed <eng.shakil8838@gmail.com>
2018-04-26 22:36:08 +0600

    logout is available

--- app/src/main/java/com/example/firebaseauth/DaMain.java
+++ app/src/main/java/com/example/firebaseauth/DaMain.java
@@ -1,13 +1,34 @@
 package com.example.firebaseauth;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class DaMain extends AppCompatActivity {
+
+    Button logoutBtn;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_da_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        logoutBtn = (Button) findViewById(R.id.logoutBtn);
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                Intent intent = new Intent(DaMain.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
